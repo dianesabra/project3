f7761101ca12f0b19084f43d8a419d83f506ce8b
Shank <shank@Nomans-MacBook-Pro.local>
2019-05-07 21:36:39 -0500

    csschanges2

--- freshoffthetable/client/src/App.js
+++ freshoffthetable/client/src/App.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Customer from './pages/Customer';
 import Cook from './pages/Cook';
 import NoMatch from './pages/NoMatch';
-import LeftPanel from './component/leftpanel';
-import MiniDrawer from './component/HomePage';
-=======
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "./pages/Main";
-import Login from "./pages/Login";
-import Customer from "./pages/Customer";
-import Cook from "./pages/Cook";
-import NoMatch from "./pages/NoMatch";
-import Cart from "./pages/Cart";
-import Signup from "./pages/Signup";
+import Cart from './pages/Cart';
+import Signup from './pages/Signup';
 // import MiniDrawer from "./component/HomePage";
->>>>>>> 9af5f876dd066851081925b568a287f8300544c3
 
 import './App.css';
 const styles = (theme) => ({
@@ -54,8 +40,8 @@ class App extends Component {
 							<Route exact path="/main" component={Main} />
 							<Route exact path="/requests" component={Customer} />
 							<Route exact path="/cook" component={Cook} />
-              <Route exact path="/cart" component={Cart} />
-          <Route exact path="/signup" component={Signup} />
+							<Route exact path="/cart" component={Cart} />
+							<Route exact path="/signup" component={Signup} />
 							<Route component={NoMatch} />
 						</Switch>
 					</main>
